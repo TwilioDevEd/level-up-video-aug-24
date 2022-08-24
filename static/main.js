40dd4ddacd8768ed1a4f4fbece382d8dee2f7286
Sarah Stringer <sstringer@twilio.com>
2022-08-24 11:05:07 -0700

    Code from Level Up live coding.

--- static/main.js
+++ static/main.js
@@ -12,15 +12,53 @@ window.addEventListener("load", () => {
   async function connect() {
     startDiv.style.display = "none";
     // TODO: Fetch an access token
+    const response = await fetch("/token", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({identity: identityInput.value})
+    });
+    const { token } = await response.json();
 
     // TODO: Use the access token to join a room
+    const room = await Twilio.Video.connect(token, {
+      video: true,
+      audio: false
+    });
+    
+    handleConnectedParticipant(room.localParticipant);
+    room.participants.forEach(handleConnectedParticipant);
+    room.on("participantConnected", handleConnectedParticipant);
+
+    room.on("participantDisconnected", handleDisconnectedParticipant);
+    window.addEventListener("pagehide", () => {room.disconnect()});
+    window.addEventListener("beforeunload", () => {room.disconnect()})
   }
 
   // TODO: Complete function for handling when a participant connects to the room
-  function handleConnectedParticipant(participant) {}
+  function handleConnectedParticipant(participant) {
+    findNextAvailableYarn(participant);
+    participant.tracks.forEach((trackPublication) => {
+      handleTrackPublished(trackPublication, participant);
+    });
+    participant.on("trackPublished", (trackPublication) => {
+      handleTrackPublished(trackPublication, participant);
+    });
+  }
 
   // TODO: Complete function for handling when a new participant track is published
-  function handleTrackPublished(trackPublication, participant) {}
+  function handleTrackPublished(trackPublication, participant) {
+    const yarn = document.getElementById(`yarn-${participant.number}`);
+    function handleTrackSubscribed(track) {
+      yarn.appendChild(track.attach());
+    }
+    if (trackPublication.track) {
+      handleTrackSubscribed(trackPublication.track);
+    }
+    trackPublication.on("subscribed", handleTrackSubscribed);
+  }
 
   // tidy up helper function for when a participant disconnects
   // or closes the page
